450939895d74706d9e0db2fe5c74398495e6ce85
Ikrom <ikromyusupov09@gmail.com>
2022-05-30 16:42:50 +0500

    three variables furnished to three values in var03

--- var03.py
+++ var03.py
@@ -1,5 +1,6 @@
 # Create a variable x with the data type integer.
-
+x = 1
 # Create a variable y with the data type float.
-
+y = 5.3
 # Create a variable z with the data type string.
+z = 'nothing'
